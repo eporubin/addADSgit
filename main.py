472685212877b717d4db483e839d351684698bc0
Elena <eporubin@gmail.com>
2023-01-24 12:29:17 +0100

    Uploaded the Script

---
+++ main.py
@@ -0,0 +1,28 @@
+#reading from input file
+f = open('input.txt')
+raw_lines = f.readlines()
+lines = [x.rstrip() for x in raw_lines]
+
+#reading from the target
+t = open('target.txt')
+t_raw_lines = t.readlines()
+t_lines = [x.rstrip() for x in t_raw_lines]
+
+
+
+end_line = t_lines.index("! $document blocks") 
+print(end_line)
+
+block_list = t_lines[1:end_line]
+for line in block_list[:10]:
+    print(line)
+
+result_list = sorted(list(set(lines + block_list)))
+
+o = open("output.txt","w")
+o.write(t_lines[0] + "\n")
+for line in result_list:
+    o.write(line + "\n")
+for line in t_lines[end_line:]:
+    o.write(line + "\n")
+
